131e659923631b050dd9d787e7723c4d9355cce1
michae1michae1 <148255109+michae1michae1@users.noreply.github.com>
2023-12-06 17:17:52 -0800

    Add files via upload

---
+++ AOC_2023/Day 1/AOC_Day1P1.py
@@ -0,0 +1,42 @@
+
+#AOC DAY 1
+#PART 1 OF 2
+
+AOC_DATA = []
+
+with open('AOC_2023\AOC_DATA.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            AOC_DATA.append(line.strip())
+
+def translate_and_combine(input_string):
+    # Extract numbers from the input string
+    numbers = [c for c in input_string if c.isdigit()]
+
+    # Combine the first and last numbers to create a new integer if possible
+    if numbers:
+        result = int(numbers[0] + numbers[-1])
+        return result
+    else:
+        return 0  # Return 0 when there are no numbers
+
+# Example data:
+input_data = [
+    '1abc2',
+    'pqr3stu8vwx',
+    'a1b2c3d4e5f',
+    'treb7uchet'
+]
+
+# Accumulate the sum of all the results
+total_sum = 0
+
+# Apply the function to each line and print the results
+for line in AOC_DATA:
+    output = translate_and_combine(line)
+    total_sum += output
+    print(f"Input string: {line}, Result: {output}")
+
+# Print the total sum of all results
+print("Total Sum:", total_sum)
+
---
+++ AOC_2023/Day 1/AOC_Day1P2.py
@@ -0,0 +1,67 @@
+
+#AOC DAY 1
+#PART 2 OF 2
+
+AOC_DATA = []
+
+with open('AOC_2023\AOC_DATA.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            AOC_DATA.append(line.strip())
+
+
+word_to_num = {
+    'one': 1, 'two': 2, 'three': 3,
+    'four': 4, 'five': 5, 'six': 6,
+    'seven': 7, 'eight': 8, 'nine': 9
+}
+
+# Create a new dictionary with modified strings
+word_to_num_modified = {word: f"{word[0]}{num}{word[-1]}" for word, num in word_to_num.items()}
+
+Ex_data = [
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrsts6xteen"
+]
+
+combos = []
+
+for line in AOC_DATA:
+    # Replace words with modified strings
+    for word, replacement in word_to_num_modified.items():
+        line = line.replace(word, replacement)
+
+    # Find the first and second integers
+    Fv = None
+    Sv = None
+    found_first = False
+
+    for char in line:
+        if char.isdigit():
+            if not found_first:
+                Fv = int(char)
+                found_first = True
+            else:
+                Sv = int(char)
+                
+    # If Sv is None, set Sv to the same number as Fv
+    if Sv is None:
+        Sv = Fv
+        
+    # Calculate Combo and store in a list
+    Combo = str(Fv) + (str(Sv) if Sv is not None else "0") if Fv is not None else None
+    combos.append(Combo)
+
+    
+
+    # Print the results for each line
+    print(f"Original: {line}, Fv: {Fv}, Sv: {Sv}, Combo: {Combo}")
+
+# Calculate and print the total
+total = sum(map(int, filter(None, combos)))
+print(f"Total: {total}")
